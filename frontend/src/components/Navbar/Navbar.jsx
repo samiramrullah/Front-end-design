2185e7f43cc0377a6bb44fbd74ab60e9d2c2b79f
tejenpradhan <tejenpradhan1@gmail.com>
2022-07-04 03:03:22 +0545

    home page initial

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './Navbar.css'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark navbar bg-dark navbar-light " id="navbar">
+        <div className="container">
+          <a className="navbar-brand" href="/">JOJO<span>Rs</span></a>
+          <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="fa fa-bars"></span> Menu
+          </button>
+          <div className="navbar-collapse collapse justify-content-end" id="nav"  >
+            <ul className="navbar-nav ms-autos">
+              <li className="nav-item active"><a href="Home" className="nav-link">Home</a></li>
+              <li className="nav-item"><a href="About" className="nav-link">About</a></li>
+              <li className="nav-item"><a href="Services" className="nav-link">Services</a></li>
+              
+              <li className="nav-item"><a href="Contact Us" className="nav-link">Contact</a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+    </div>
   )
 }
 
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,40 @@
 import './Home.css'
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <div className='main-wrap' style={{height: '490px' ,background: '50%' ,position:'117px' }} >
+        <div className='overlay'></div>
+        <section>
+      <div className='container'>
+        <div className='row justify-content-end ms-auto'  style={{ height: '170px', }}>
+
+          <div className='col-lg-4 '>
+            <div className="card " style={{ width: '300px', marginTop:'9rem' }}>
+              <div className="card-body">
+                <h5 className="card-title">Card title</h5>
+                <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      </section>
+      <section style={{zIndex:1}}>
+      <div className='container'>
+          <div className='row no-gutters slider-text  align-items-center justify-content-start'  style={{ height: '429px', }}>
+            <div className='col-md-7'>
+              <h2>Welcome to Vacation Rental</h2>
+            </div>
+          </div>
+
+        </div>
+      </section>
+        
+      </div>
+     
+      
+    </div>
   )
 }
 
