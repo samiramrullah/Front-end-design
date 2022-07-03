47987255bbc2b0b1ef4209d60e7b6659445002b8
samiramrullah <samiramrullah@gmail.com>
2022-07-04 01:50:31 +0530

    with auth token

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,22 @@
 import Home from './pages/Home/Home';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
+import { Routes, Route } from 'react-router-dom';
+import Signup from './pages/Signup/Signup';
+import Login from './pages/Login/Login';
 import './App.css';
 function App() {
   return (
     <div className="App">
-      App
+     <Navbar/>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/dashbord' element={<Home/>}/>
+     </Routes>
+     {/* <Signup/>
+     <Footer/> */}
     </div>
   );
 }
