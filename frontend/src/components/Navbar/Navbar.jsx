ea8231c572e56afa6f5d991ae694a9871e80a59c
tejenpradhan <tejenpradhan1@gmail.com>
2022-07-04 03:06:56 +0545

    nav conflicts

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react'
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './Navbar.css'
@@ -26,65 +25,5 @@ const Navbar = () => {
     </div>
   )
 }
-=======
-import { NavLink } from "react-router-dom";
-// import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-
-const Navbar = () => {
-  return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <NavLink className="navbar-brand" to="/">
-          CredAdda
-        </NavLink>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item active">
-              <NavLink className="nav-link" to={"/"}>
-                Home{" "}
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link" to={"/about"}>
-                About
-              </NavLink>
-            </li>
-
-            <li className="nav-item">
-              <NavLink className="nav-link" to={"/dashboard"}>
-                Contact
-              </NavLink>
-            </li>
-
-            <li className="nav-item">
-              <NavLink className="nav-link" to={"/login"}>
-                Sign in
-              </NavLink>
-            </li>
-
-            <li className="nav-item">
-              <NavLink className="nav-link" to={"/signup"}>
-                Register
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </nav>
-    </>
-  );
-};
->>>>>>> 65d4b170023f04235c7d76a43b31b4f0cd176b40
 
 export default Navbar;
