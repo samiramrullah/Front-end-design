65d4b170023f04235c7d76a43b31b4f0cd176b40
samiramrullah <samiramrullah@gmail.com>
2022-07-04 02:44:57 +0530

    with navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import Home from './pages/Home/Home';
-import Navbar from './components/Navbar/Navbar';
+import Navbar from './components/Navbar/Navbar'
 import Footer from './components/Footer/Footer';
 import { Routes, Route } from 'react-router-dom';
 import Signup from './pages/Signup/Signup';
 import Login from './pages/Login/Login';
 import './App.css';
+import Dashboard from './pages/Dashboard/Dashboard';
 function App() {
   return (
     <div className="App">
@@ -13,7 +14,7 @@ function App() {
       <Route path='/' element={<Home/>}/>
       <Route path='/signup' element={<Signup/>}/>
       <Route path='/login' element={<Login/>}/>
-      <Route path='/dashbord' element={<Home/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
      </Routes>
      {/* <Signup/>
      <Footer/> */}
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
-import './Navbar.css'
+import { NavLink } from "react-router-dom";
+// import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <NavLink className="navbar-brand" to="/">
+          CredAdda
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-export default Navbar
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item active">
+              <NavLink className="nav-link" to={"/"}>
+                Home{" "}
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to={"/about"}>
+                About
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link" to={"/dashboard"}>
+                Contact
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link" to={"/login"}>
+                Sign in
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink className="nav-link" to={"/signup"}>
+                Register
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
