8965f167c2843b6c7795c88cdf777c7b07a85eeb
samiramrullah <samiramrullah@gmail.com>
2022-07-07 01:30:37 +0530

    Home page Ui changed

--- frontend/src/components/Post/Post.jsx
+++ frontend/src/components/Post/Post.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 import Infocard from '../../utils/Helpers/Infocard';
 import { useSelector, useDispatch } from 'react-redux';
 import { setpostStatus } from '../../redux/PostSlice'
-import { DollarCircleOutlined,ArrowRightOutlined} from '@ant-design/icons';
+import { DollarCircleOutlined, ArrowRightOutlined } from '@ant-design/icons';
 
 const Post = () => {
     const [countryToSend, setcountryToSend] = useState();
@@ -44,41 +44,41 @@ const Post = () => {
                         <div className='container mt-5'>
                             <div className='row my-5'>
                                 <div className="col">
-                                <DollarCircleOutlined  style={{ fontSize: '4rem', color: '#757573' }} />
+                                    <DollarCircleOutlined style={{ fontSize: '4rem', color: '#757573' }} />
                                 </div>
                                 <div className="col">
-                                <ArrowRightOutlined style={{ fontSize: '4rem', color: '#757573' }} />
+                                    <ArrowRightOutlined style={{ fontSize: '4rem', color: '#757573' }} />
                                 </div>
                                 <div className="col">
-                                <DollarCircleOutlined style={{ fontSize: '4rem', color: '#757573' }} />
+                                    <DollarCircleOutlined style={{ fontSize: '4rem', color: '#757573' }} />
                                 </div>
                             </div>
-                        <form className=' row mt-5 mb-3  d-flex align-items-center' onSubmit={onSubmitHandler}>
-                            <div class="form-group row">
-                                <div className='col'>
-                                    <select className="form-select" aria-label="Default select example" onChange={(e) => setcountryToSend(e.target.value)}>
-                                        <option>India</option>
-                                        <option>Chanda</option>
-                                    </select>
+                            <form className=' row mt-5 mb-3  d-flex align-items-center' onSubmit={onSubmitHandler}>
+                                <div class="form-group row">
+                                    <div className='col'>
+                                        <select className="form-select" aria-label="Default select example" onChange={(e) => setcountryToSend(e.target.value)}>
+                                            <option>India</option>
+                                            <option>Chanda</option>
+                                        </select>
+                                    </div>
+                                    <div className='col'>
+                                        <input type={'number'} class="form-control mb-2" placeholder="Enter amount" required onChange={(e) => setamount(e.target.value)} />
+                                        <small id="emailHelp" className="form-text text-muted mt-2">We'll never share your email with anyone else.</small>
+                                    </div>
+
+                                    <div className='col'>
+                                        <select className="form-select" aria-label="Default select example" onChange={(e) => setcountryToRecieve(e.target.value)}>
+                                            <option>Canada</option>
+                                            <option>India</option>
+                                        </select>
+                                    </div>
+
+
                                 </div>
-                                <div className='col'>
-                                    <input type={'number'} class="form-control mb-2" placeholder="Enter amount" required onChange={(e) => setamount(e.target.value)} />
-                                    <small id="emailHelp" className="form-text text-muted mt-2">We'll never share your email with anyone else.</small>
-                                </div>
-
-                                <div className='col'>
-                                    <select className="form-select" aria-label="Default select example" onChange={(e) => setcountryToRecieve(e.target.value)}>
-                                        <option>Canada</option>
-                                        <option>India</option>
-                                    </select>
-                                </div>
 
 
-                            </div>
-
-                           
-                        </form>
-                        <div className='row d-flex justify-content-center'>
+                            </form>
+                            <div className='row d-flex justify-content-center'>
                                 <div className='col-6 col-md-4'>
                                     <button type="submit" class="btn btn-primary">Submit</button>
                                 </div>
@@ -86,8 +86,8 @@ const Post = () => {
                             </div>
                         </div>
 
-                        
-                        
+
+
                     </div>
                 </div>
             )}
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -4,12 +4,61 @@ const Home = () => {
   return (
     <HomeWrapper>
       <div className='main-wrap' style={{ height: '490px', background: '50%', position: '117px' }} >
-        <div className='overlay'></div>
+        {/* <div className='overlay'></div> */}
+
         <section>
           <div className='container'>
-            <div className='row justify-content-end ms-auto' style={{ height: '170px', }}>
-              <div className='col-lg-4 '>
-                <div className="card " style={{ width: '300px', marginTop: '9rem' }}>
+            <div className='row' >
+              <div className="col-8 ml-5 " style={{ marginTop: '6rem' }}>
+
+                <div className="row  d-flex align-items-end">
+                  <h1 className='title'>Welcome to Cred Adda</h1>
+                </div>
+                <form className=' row mt-5 mb-3  d-flex align-items-center' >
+                  <div class="form-group row">
+                    <div className='col'>
+                      <select className="form-select" aria-label="Default select example" >
+                        <option>India</option>
+                        <option>Chanda</option>
+                      </select>
+                    </div>
+                    <div className='col'>
+                      <input type={'number'} class="form-control mb-2" placeholder="Enter amount" required />
+
+                    </div>
+
+                    <div className='col'>
+                      <select className="form-select" aria-label="Default select example" >
+                        <option>Canada</option>
+                        <option>India</option>
+                      </select>
+                    </div>
+                    <div className='col d-flex justify-content-center '>
+                      <button type="submit" class="btn btn-outline-dark">Submit</button>
+                    </div>
+
+                  </div>
+
+
+                </form>
+
+              </div>
+              <div className='col-auto'>
+                <div className="card " style={{ width: '300px', marginTop: '5rem' }}>
+                  <div className="card-header">
+                    <div className="container d-flex justify-content-center my-1">
+                      <div class="btn-group btn-group-toggle" data-toggle="buttons">
+                        <button class="btn btn-outline-dark active">
+                          <span type="radio" name="options" id="option1" autocomplete="off" checked /> Sign In
+                        </button>
+                        <button class="btn btn-outline-dark">
+                          <span type="radio" name="options" id="option2" autocomplete="off" /> Log In
+                        </button>
+
+                      </div>
+                    </div>
+
+                  </div>
                   <div className="card-body">
                     <h5 className="card-title">Card title</h5>
                     <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
@@ -20,15 +69,7 @@ const Home = () => {
             </div>
           </div>
         </section>
-        <section style={{ zIndex: 1 }}>
-          <div className='container'>
-            <div className='row no-gutters slider-text  align-items-center justify-content-start' style={{ height: '429px', }}>
-              <div className='col-md-7'>
-                <h2>Welcome to Cred Adda</h2>
-              </div>
-            </div>
-          </div>
-        </section>
+
       </div>
     </HomeWrapper>
   )
