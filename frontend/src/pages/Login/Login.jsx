f3500ec5e9ae1e807d793595ceeb854ec933843e
samiramrullah <samiramrullah@gmail.com>
2022-07-07 15:34:16 +0530

    with login image

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -27,24 +27,34 @@ const Login = () => {
   }
   return (
     <HomeWrapper>
-      <center className='container'>
-        <div className="card loginmain" style={{ width: '40%' }}>
-          <div className="card-body">
-            <h5 className="card-title">Log In</h5>
-            <form onSubmit={onSubmitLogin}>
-              <div class="form-group">
-                <label>Email address</label>
-                <input type="email" class="form-control" placeholder="Enter email" required onChange={(e) => setemail(e.target.value)} />
+      <div className='container'>
+        <div className="row">
+          <div className="col">
+            <div>
+              <div className="card">
+                <div className="card-body">
+                  <h5 className="card-title">Welcome Back to Cred Adda</h5>
+                  <form onSubmit={onSubmitLogin}>
+                    <div class="form-group">
+                      <label>Email address</label>
+                      <input type="email" class="form-control" placeholder="Enter email" required onChange={(e) => setemail(e.target.value)} />
+                    </div>
+                    <div class="form-group">
+                      <label>Password</label>
+                      <input type="password" class="form-control" placeholder="Password" required onChange={(e) => setpassword(e.target.value)} />
+                    </div>
+                    <button type="submit" class="btn btn-primary mt-3">Login</button>
+                  </form>
+                </div>
               </div>
-              <div class="form-group">
-                <label>Password</label>
-                <input type="password" class="form-control" placeholder="Password" required onChange={(e) => setpassword(e.target.value)} />
-              </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
+            </div>
           </div>
+          <center className="col">
+            <img src='https://wallpaperaccess.com/full/2675409.jpg' width="808" height="458" alt="Lobby Image" />
+          </center>
         </div>
-      </center>
+      </div>
+
     </HomeWrapper>
   )
 }
