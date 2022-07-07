47997d593dabc271edaf0bee4f94d7d94bc79e91
samiramrullah <samiramrullah@gmail.com>
2022-07-07 13:15:23 +0530

    with about page

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,8 +1,56 @@
 import React from 'react'
-import HomeWrapper from '../../components/HomeWrapper'
+import HomeWrapper from '../../components/HomeWrapper';
+import { NavLink } from 'react-router-dom';
+import './About.css';
 const About = () => {
   return (
-    <HomeWrapper>About</HomeWrapper>
+    <HomeWrapper>
+      <div className="AboutMain">
+        <div class="we-are-block">
+
+          <div id="about-us-section">
+
+            <div class="about-us-image">
+
+              <img src="https://wallpaperaccess.com/full/2675409.jpg" width="808" height="458" alt="Lobby Image" />
+
+            </div>
+
+            <div class="about-us-info">
+
+              <h2>Cred Adda</h2>
+
+              <p>We make money transer easy, fast and smooth</p>
+
+              <NavLink to={'/signup'} title="About Us Button">Connect With US</NavLink>
+
+            </div>
+
+          </div>
+
+          <div id="history-section">
+
+            <div class="history-image">
+
+              <img src="https://thumbs.dreamstime.com/b/canada-indian-flag-hands-together-man-hand-beautiful-color-red-wonderful-place-126029930.jpg" width="951" height="471" alt="Building Pic" />
+
+            </div>
+
+            <div class="history-info">
+
+              <h2>Why Cred Adda</h2>
+
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maxime in magni explicabo mollitia. Consequatur cumque provident molestias quos, doloribus iure dolorum nisi error ad aliquam laboriosam voluptatum mollitia maxime magni.</p>
+
+              <a href="#" title="History Button">Contact US</a>
+
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+    </HomeWrapper>
   )
 }
 
