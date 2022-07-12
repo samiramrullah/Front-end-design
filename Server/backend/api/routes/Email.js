ac5580b0ce98f3f30d21ebf7f6d315398c420298
samiramrullah <samiramrullah@gmail.com>
2022-07-12 20:17:59 +0530

    language set antd

--- Server/backend/api/routes/Email.js
+++ Server/backend/api/routes/Email.js
@@ -3,28 +3,26 @@ const nodemailer = require("nodemailer");
 const router = express.Router();
 
 router.post("/", (req, res, next) => {
-    var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: 'myemail  ',
-          pass: 'app password'
-        }
-      });
-      
-      var mailOptions = {
-        from: 'myemail',
-        to: 'sender email',
-        subject: 'Sending with modemailer',
-        text: 'Hello World!'
-      };
-      
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log("error");
-        } else {
-          console.log('Successfully Sent');
-        }
-      });
+  var mail = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "jayj10137@gmail.com",
+      pass: "Demo@123",
+    },
+  });
+  var mailOption = {
+    from: "jayj10137@gmail.com",
+    to: "samiramrullah@gmail.com",
+    subject: "sending demo mail",
+    text: `hello`,
+  };
+  mail.sendMail(mailOption, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent" + info.response);
+    }
+  });
   res.status(200).json({
     message: "Message Sent",
   });
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,18 +3,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import {Provider} from 'react-redux';
+import { Provider } from "react-redux";
 import store from "./redux/Store";
+import { ConfigProvider } from "antd";
+import pt_PT from "antd/es/locale/fr_FR";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import 'antd/dist/antd.css';
+import "antd/dist/antd.css";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Provider store={store}>
-      <App />
+      <Provider store={store}>
+        <ConfigProvider locale={pt_PT}>
+          <App />
+        </ConfigProvider>
       </Provider>
     </BrowserRouter>
   </React.StrictMode>
