7c0b9babaad9bc28a7ae71a49cc6936a79ceeade
samiramrullah <samiramrullah@gmail.com>
2022-07-19 08:16:10 +0530

    with delete modal

--- Server/backend/api/routes/User.js
+++ Server/backend/api/routes/User.js
@@ -75,7 +75,6 @@ router.post("/signup", (req, res, next) => {
 
 // upload.single('UserImage')
 router.post("/:userId", async (req, res, next) => {
-  console.log('here');
   try {
     const userId = req.params.userId;
     console.log(req.body);
