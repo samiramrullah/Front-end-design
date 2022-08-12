f2b7d6b81f63a0915dbd09b3248a5f740a2731cc
samiramrullah <samiramrullah@gmail.com>
2022-08-12 22:30:49 +0530

    email link

--- Server/backend/api/routes/Email.js
+++ Server/backend/api/routes/Email.js
@@ -3,29 +3,30 @@ const nodemailer = require("nodemailer");
 const router = express.Router();
 
 router.post("/", (req, res, next) => {
-  var mail = nodemailer.createTransport({
-    service: "gmail",
+
+  let mailTransporter = nodemailer.createTransport({
+    service: 'gmail',
     auth: {
-      user: "startup.credadda@gmail.com",
-      pass: "mvcvpvuytacwmcbg",
-    },
-    tls:{rejectUnauthorized:false}
-  });
-  var mailOption = {
-    from: "startup.credadda@gmail.com",
-    to: "samiramrullah@gmail.com",
-    subject: "sending demo mail",
-    text: `hello`,
-  };
-  mail.sendMail(mailOption, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log("Email sent" + info.response);
+      user: 'startup.credadda@gmail.com',
+      pass: 'Happylearning@2022'
     }
   });
-  res.status(200).json({
-    message: "Message Sent",
+
+  let mailDetails = {
+    from: 'startup.credadda@gmail.com',
+    to: 'samiramrullah@gmail.com',
+    subject: 'Test mail',
+    text: 'Node.js testing mail for GeeksforGeeks'
+  };
+
+  mailTransporter.sendMail(mailDetails, function (err, data) {
+    if (err) {
+      console.log('Error Occurs');
+    } else {
+      console.log('Email sent successfully');
+    }
   });
+
+
 });
 module.exports = router;
